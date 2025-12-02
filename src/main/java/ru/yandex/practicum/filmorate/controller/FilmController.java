26a4e4c0e93a54ce0848e0ed6a55487341c2976b
Misha Plehanov <mih.plekhanov@yandex.ru>
2025-12-03 00:16:07 +0300

    Добавить валидацию для класса FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,8 +1,10 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,12 +12,22 @@ import java.util.List;
 @RestController
 @RequestMapping("/films")
 public class FilmController {
+    private static final LocalDate MIN_DATE_RELEASE = LocalDate.of(1985, 12, 28);
     private final HashMap<Long, Film> films = new HashMap<>();
 
     @PostMapping
     public Film addFilm(@RequestBody Film film) {
         if(film.getName() == null) {
-
+            throw new ValidationException("Название не может быть пустым");
+        }
+        if(film.getDescription().length() > 200) {
+            throw new ValidationException("Максимальная длина описания — 200 символов");
+        }
+        if(film.getReleaseDate().isAfter(MIN_DATE_RELEASE)) {
+            throw new ValidationException("Дата релиза — не раньше 28 декабря 1895 года");
+        }
+        if(film.getDuration().isNegative()) {
+            throw new ValidationException("Продолжительность фильма должна быть положительным числом.");
         }
         Film newFilm = new Film();
         newFilm.setId(nextIdGenerate());
