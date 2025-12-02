1ce98d9b16c851034e5d4c487c482579177ec653
Misha Plehanov <mih.plekhanov@yandex.ru>
2025-12-03 02:48:20 +0300

    Добавить логирование slf4j для FilmCont

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -9,24 +9,34 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 @RestController
 @RequestMapping("/films")
 public class FilmController {
+    private final static Logger log = LoggerFactory.getLogger(FilmController.class);
+
     private static final LocalDate MIN_DATE_RELEASE = LocalDate.of(1985, 12, 28);
     private final HashMap<Long, Film> films = new HashMap<>();
 
     @PostMapping
     public Film addFilm(@RequestBody Film film) {
         if(film.getName() == null) {
+            log.warn("Указанное некорректное имя фильма {} при создании.", "<пусто>");
             throw new ValidationException("Название не может быть пустым");
         }
         if(film.getDescription().length() > 200) {
+            log.warn("Превышена длинна описания {} при создании.", film.getDescription().length());
             throw new ValidationException("Максимальная длина описания — 200 символов");
         }
         if(film.getReleaseDate().isAfter(MIN_DATE_RELEASE)) {
+            log.warn("Указана неверная дата релиза, при создании, дата раньше допустимого значения {}",
+                        film.getReleaseDate());
             throw new ValidationException("Дата релиза — не раньше 28 декабря 1895 года");
         }
         if(film.getDuration().isNegative()) {
+            log.warn("Продолжительность отрицательная {} при создании.", film.getDescription().length());
             throw new ValidationException("Продолжительность фильма должна быть положительным числом.");
         }
         Film newFilm = new Film();
@@ -37,7 +47,7 @@ public class FilmController {
         newFilm.setDuration(film.getDuration());
 
         films.put(newFilm.getId(), newFilm);
-
+        log.info("Фильм с названием {} создан.", film.getName());
         return newFilm;
     }
 
@@ -65,6 +75,7 @@ public class FilmController {
         } else if(!film.getReleaseDate().isBefore(MIN_DATE_RELEASE)) {
             newFilm.setReleaseDate(film.getReleaseDate());
         } else {
+            log.warn("Неверная дата релиза фильма {}, при обновлении, {}", film.getName(), film.getReleaseDate());
             throw new ValidationException("Дата релиза — не раньше 28 декабря 1895 года");
         }
 
@@ -73,11 +84,13 @@ public class FilmController {
         } else if (film.getDuration().toMinutes() > 0) {
             newFilm.setDuration(film.getDuration());
         } else {
+            log.warn("Отрицательная продолжительность фильма {}, указали {} при обновлении",
+                        film.getName(), film.getDuration());
             throw new ValidationException("Продолжительность фильма должна быть положительным числом.");
         }
 
         films.put(newFilm.getId(), newFilm);
-
+            log.info("Фильм {} обновлен.", film.getName());
         return newFilm;
     }
 
