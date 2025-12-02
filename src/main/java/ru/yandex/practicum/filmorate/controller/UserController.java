ea161eda362a3ef9fbc5dda845685db928394f19
Misha Plehanov <mih.plekhanov@yandex.ru>
2025-12-03 02:33:19 +0300

    Добавить логирование slf4j для UserCont

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,19 +24,24 @@ public class UserController {
     @PostMapping
     public User addUser(@RequestBody User user) {
         if(user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+            log.warn("Пользователь {} указал неверный email {} при регистрации.", user.getName(), user.getEmail());
             throw new ValidationException("Электронная почта не может быть пустой и должна содержать символ @");
         }
 
         if(user.getLogin() == null || user.getLogin().contains(" ") || user.getLogin().isBlank()) {
+            log.warn("Пользователь {} указал неверный логин {} при регистрации.", user.getName(), user.getLogin());
             throw new ValidationException("Логин не может быть пустым и содержать пробелы");
         }
 
         User newUser = new User();
         if(user.getName() == null || user.getName().isBlank() || user.getName().contains(" ")) {
+            log.warn("Пользователю {} назначено имя {} при регистрации.", user.getName(), user.getLogin());
             newUser.setName(user.getLogin());
         }
 
         if(user.getBirthday() == null || user.getBirthday().isAfter(LocalDate.now())) {
+            log.warn("Пользователь {} указал неверную дату рождения {} при регистрации.",
+                        user.getName(), user.getBirthday());
             throw new ValidationException("Дата рождения не может быть в будущем.");
         }
 
@@ -48,7 +54,7 @@ public class UserController {
         newUser.setBirthday(user.getBirthday());
 
         users.put(newUser.getId(), newUser);
-
+        log.info("Пользователь с именем {} и логином {} зарегистрирован.", user.getName(), user.getLogin());
         return newUser;
     }
 
@@ -68,6 +74,7 @@ public class UserController {
         if(user.getEmail() == null) {
             newUser.setEmail(oldUser.getEmail());
         } else if(user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+            log.warn("Пользователь {} указал неверный email {} при редактировании.", user.getName(), user.getEmail());
             throw new ValidationException("Электронная почта не может быть пустой и должна содержать символ @");
         } else {
             newUser.setEmail(user.getEmail());
@@ -76,6 +83,7 @@ public class UserController {
         if(user.getLogin() == null) {
             newUser.setLogin(oldUser.getLogin());
         } else if(user.getLogin().contains(" ") || user.getLogin().isBlank()) {
+            log.warn("Пользователь {} указал неверный логин {} при редактировании.", user.getName(), user.getLogin());
             throw new ValidationException("Логин не может быть пустым и содержать пробелы");
         } else {
             newUser.setLogin(user.getLogin());
@@ -84,13 +92,15 @@ public class UserController {
         if(user.getBirthday() == null) {
             newUser.setBirthday(oldUser.getBirthday());
         } else if(user.getBirthday().isAfter(LocalDate.now())) {
+            log.warn("Пользователь {} указал неверную дату рождения {} при редактировании.",
+                user.getName(), user.getBirthday());
             throw new ValidationException("Дата рождения не может быть в будущем.");
         } else {
             newUser.setBirthday(user.getBirthday());
         }
 
         users.put(newUser.getId(), newUser);
-
+        log.info("Пользователь с именем {} и логином {} обновил свой профиль.", user.getName(), user.getLogin());
         return newUser;
     }
 
