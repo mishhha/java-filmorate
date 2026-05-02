d35d8adb050f3a9ea8d0a5c8a62ff80f4f7862c3
nodir asomov <nasomov93@gmail.com>
2026-05-02 13:04:11 +0500

    adding- reviews

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/GlobalExceptionHandler.java
@@ -6,10 +6,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.yandex.practicum.filmorate.controller.ErrorHandler;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 @Slf4j
@@ -17,35 +13,30 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorHandler.ErrorResponse handlerValidation(final ValidationException e) {
-        return new ErrorHandler.ErrorResponse(e.getMessage());
+    public ErrorResponse handlerValidation(final ValidationException e) {
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorHandler.ErrorResponse handlerNotFound(final NotFoundException e) {
-        return new ErrorHandler.ErrorResponse(e.getMessage());
+    public ErrorResponse handlerNotFound(final NotFoundException e) {
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorHandler.ErrorResponse handlerServerException(final Throwable e) {
+    public ErrorResponse handlerServerException(final Throwable e) {
         log.error("Необработанное исключение", e);
-        return new ErrorHandler.ErrorResponse(
-            "Произошла непредвиденная ошибка."
+        return new ErrorResponse(
+                "Произошла непредвиденная ошибка."
         );
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error ->
-            errors.put(error.getField(), error.getDefaultMessage())
-        );
-        return errors;
+    public ErrorResponse handleValidationExceptions(MethodArgumentNotValidException ex) {
+        return new ErrorResponse("Validation error");
     }
-
 
 
 }
