a21793e495a3f099a64a0b7a71f965a5f732b091
nodir asomov <nasomov93@gmail.com>
2026-05-05 12:07:50 +0500

    fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,50 +1,76 @@
--- Жанры
-INSERT INTO genres (id, name) VALUES
-    (1, 'Комедия'),
-    (2, 'Драма'),
-    (3, 'Мультфильм'),
-    (4, 'Триллер'),
-    (5, 'Документальный'),
-    (6, 'Боевик');
+-- ЖАНРЫ
+INSERT INTO genres (name) VALUES
+('Комедия'),
+('Драма'),
+('Мультфильм'),
+('Триллер'),
+('Документальный'),
+('Боевик');
 
--- Рейтинги
-INSERT INTO mpa_ratings (id, name) VALUES
-    (1, 'G'),
-    (2, 'PG'),
-    (3, 'PG-13'),
-    (4, 'R'),
-    (5, 'NC-17');
+-- РЕЙТИНГИ
+INSERT INTO mpa_ratings (name) VALUES
+('G'),
+('PG'),
+('PG-13'),
+('R'),
+('NC-17');
 
--- Статусы дружбы
+-- СТАТУСЫ ДРУЖБЫ
 INSERT INTO friendship_statuses (id, name) VALUES
-    (1, 'CONFIRMED'),
-    (2, 'UNCONFIRMED');
+(1, 'CONFIRMED'),
+(2, 'UNCONFIRMED');
 
--- Пользователи
-INSERT INTO users (id, name, email, login, birthday) VALUES
-    (1, 'Test User', 'test@mail.ru', 'test_login', '1990-01-01'),
-    (2, 'Second User', 'second@mail.ru', 'second_login', '1992-03-03'),
-    (3, 'Third User', 'third@mail.ru', 'third_login', '1995-05-05');
+-- ПОЛЬЗОВАТЕЛИ (БЕЗ ID!)
+INSERT INTO users (name, email, login, birthday) VALUES
+('Test User', 'test@mail.ru', 'test_login', '1990-01-01'),
+('Second User', 'second@mail.ru', 'second_login', '1992-03-03'),
+('Third User', 'third@mail.ru', 'third_login', '1995-05-05');
 
--- Фильмы
-INSERT INTO films (id, name, description, release_date, duration, mpa_rating_id)
+-- ФИЛЬМЫ (БЕЗ ID + likes_count = 0)
+INSERT INTO films (name, description, release_date, duration, mpa_rating_id, likes_count)
 VALUES
-    (1, 'Test Film', 'Test Description', '2020-01-01', 120, 1),
-    (2, 'Бриллиантовая рука', 'Test Description2', '1969-01-01', 94, 2),
-    (3, 'Чебурашка', 'Test Description3', '2023-01-01', 113, 3),
-    (4, 'Стражи Галактики', 'Test Description4', '2023-01-01', 121, 3);
+('Test Film', 'Test Description', '2020-01-01', 120, 1, 0),
+('Бриллиантовая рука', 'Test Description2', '1969-01-01', 94, 2, 0),
+('Чебурашка', 'Test Description3', '2023-01-01', 113, 3, 0),
+('Стражи Галактики', 'Test Description4', '2023-01-01', 121, 3, 0);
 
--- Лайки
+-- ЛАЙКИ
 INSERT INTO likes (film_id, user_id)
 VALUES
-    (2, 1),
-    (3, 1),
-    (4, 1),
-    (2, 2),
-    (3, 2),
-    (2, 3),
-    (1, 3);
+(2, 1),
+(3, 1),
+(4, 1),
+(2, 2),
+(3, 2),
+(2, 3),
+(1, 3);
 
--- Жанры фильмов
+-- СИНХРОНИЗАЦИЯ likes_count
+UPDATE films f
+SET likes_count = (
+    SELECT COUNT(*)
+    FROM likes l
+    WHERE l.film_id = f.id
+);
+
+-- ЖАНРЫ ФИЛЬМОВ
 INSERT INTO film_genres (film_id, genre_id)
-VALUES (1, 1);
+VALUES
+(1, 1),
+(2, 2),
+(3, 1),
+(4, 6);
+
+-- РЕЖИССЁРЫ
+INSERT INTO directors (name) VALUES
+('Стивен Спилберг'),
+('Кристофер Нолан'),
+('Квентин Тарантино');
+
+-- СВЯЗЬ ФИЛЬМЫ-РЕЖИССЁРЫ
+INSERT INTO films_directors (film_id, director_id)
+VALUES
+(1, 1),
+(2, 2),
+(3, 1),
+(4, 3);
