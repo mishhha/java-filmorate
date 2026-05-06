696e56486a412a36ec002b75ae81290b9bd7634a
Misha Plehanov <mih.plekhanov@yandex.ru>
2026-05-07 00:16:39 +0300

    правка чекСтайла GitHub

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -88,7 +88,7 @@ public class FilmController {
         @RequestParam (value = "genreId", required = false) @Positive Long genreId,
         @RequestParam (value = "year", required = false) @Positive Long year
     ) {
-        if(genreId == null || year == null) {
+        if (genreId == null || year == null) {
             return filmService.getTopFilmsByLikes(limit.intValue());
         }
             return filmService.searchTopFilmsByGenreAndYear(limit, genreId, year);
