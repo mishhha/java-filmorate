e8006e0288b82adf395ac53be3ffa17f528456de
Misha Plehanov <mih.plekhanov@yandex.ru>
2026-05-07 01:09:45 +0300

    добавить JOIN для жанров в SQL

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -277,6 +277,7 @@ public class FilmDbStorage implements FilmStorage {
                       m.id AS rating_id,
                       m.name AS rating_name
                 FROM films f
+                JOIN film_genres AS fg ON f.id = fg.film_id
                 LEFT JOIN mpa_ratings m ON f.mpa_rating_id = m.id
                 WHERE EXTRACT(YEAR FROM f.release_date) = ?
                 ORDER BY f.likes_count DESC
