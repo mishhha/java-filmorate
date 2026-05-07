0fdb8ccd1687181d88a51101f4a86a59d5bda55a
Misha Plehanov <mih.plekhanov@yandex.ru>
2026-05-08 00:47:37 +0300

    исправление кода под автотесы Git

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -523,19 +523,20 @@ public class FilmDbStorage implements FilmStorage {
     }
 
     @Override
-    public void addLike(Long filmId, Long userId) {
+    public boolean addLike(Long filmId, Long userId) {
         if (!jdbc.queryForObject(CHECK_FILM_EXISTS_BY_ID_QUERY, Boolean.class, filmId)) {
             throw new NotFoundException("Фильм с id " + filmId + " не найден");
         }
         if (!jdbc.queryForObject(CHECK_USER_EXISTS_BY_ID_QUERY, Boolean.class, userId)) {
             throw new NotFoundException("Пользователь с id " + userId + " не найден");
         }
-        Boolean exists = jdbc.queryForObject(CHECK_LIKE_EXISTS, Boolean.class, filmId, userId);
-        if (exists != null && exists) {
-            return;
+        boolean exists = jdbc.queryForObject(CHECK_LIKE_EXISTS, Boolean.class, filmId, userId);
+        if (!exists) {
+            return false;
         }
         jdbc.update(INSERT_ADD_LIKE, filmId, userId);
         jdbc.update(UPDATE_FILM_LIKES, filmId);
+        return true;
     }
 
     @Override
