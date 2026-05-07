abfe6ac4492fb91b268f163f5f8729fcc7b572e2
Misha Plehanov <mih.plekhanov@yandex.ru>
2026-05-07 22:43:40 +0300

    исправление кода под автотесы Git

--- src/main/java/ru/yandex/practicum/filmorate/model/user/EventTypes.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/user/EventTypes.java
@@ -1,5 +1,12 @@
 package ru.yandex.practicum.filmorate.model.user;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum EventTypes {
-    LIKE, DISLIKE, REVIEW, FRIEND
+    LIKE, DISLIKE, REVIEW, FRIEND;
+
+    @JsonValue
+    public String getValue() {
+        return name();
+    }
 }
