77c14ab06f3b74801e5c3ab93e3ffda315c5c1a9
Misha Plehanov <mih.plekhanov@yandex.ru>
2026-05-08 00:20:34 +0300

    исправление кода под автотесы Git

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -17,11 +17,3 @@ INSERT INTO mpa_ratings (id, name) VALUES
 INSERT INTO friendship_statuses (id, name) VALUES
     (1, 'CONFIRMED'),
     (2, 'UNCONFIRMED');
-
--- Пользователь с id=1
-INSERT INTO users (id, name, login, email, birthday)
-VALUES (1, 'Test User', 'testuser', 'test@mail.ru', '1990-01-01');
-
--- Фильм с id=3
-INSERT INTO films (id, name, description, release_date, duration, likes_count, mpa_rating_id)
-VALUES (3, 'New film with director', 'Film with director', '1999-04-30', 120, 0, 3);
