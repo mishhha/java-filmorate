343f4074c947b0f176db8f9507718dad9a1a95a7
Misha Plehanov <mih.plekhanov@yandex.ru>
2026-05-10 00:34:43 +0300

    отладка кода после тестов Git

--- src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
@@ -101,14 +101,14 @@ public class ReviewService {
         reviewStorage.saveReaction(reviewId, userId, false);
 
         //Добавление события в историю
-        /* userStorage.addEvent(Event.builder()
+        userStorage.addEvent(Event.builder()
             .userId(userId)
             .eventType(EventTypes.DISLIKE)
             .operation(EventOperations.ADD)
             .entityId(reviewId)
             .build());
 
-         */
+
     }
 
     public void removeLike(Long reviewId, Long userId) {
@@ -127,13 +127,13 @@ public class ReviewService {
         reviewStorage.deleteReaction(reviewId, userId);
 
         //Добавление события в историю
-        /* userStorage.addEvent(Event.builder()
+        userStorage.addEvent(Event.builder()
             .userId(userId)
             .eventType(EventTypes.DISLIKE)
             .operation(EventOperations.REMOVE)
             .entityId(reviewId)
             .build());
 
-         */
+
     }
 }
